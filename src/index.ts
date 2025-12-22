c3f279fd8ce1449f9ce20c89a4a50604895a2fac
NISHIZAWA Shuntaro <me@s2n.tech>
2025-12-23 00:32:12 +0900

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { Client } from "discord.js";
+
+const client = new Client({
+	intents: ["Guilds", "GuildMessages", "MessageContent"],
+});
+
+client.on("clientReady", () => {
+	console.log(`Logged in as ${client.user?.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+	console.log(`Message received: ${message.content}`);
+});
+
+client.login(process.env["DISCORD_BOT_TOKEN"]);
